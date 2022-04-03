192f44dd8c1db7c0dff3789e1c01858b1435a6b8
RAKIBKHAN <rkrakib2335>
2022-04-03 15:07:10 +0600

    first commit

---
+++ slice.js
@@ -0,0 +1,10 @@
+let honesty = ['rifat', 'shahin', 'rakib', 'redwon', 'mukta'];
+let Selflessness = honesty.slice(2, 4);
+console.log(Selflessness);
+
+
+
+
+
+
+// 'rifat', 'shahin', 'rakib', 'redwon', 'mukta'
